ec22d17eea2d7f82ca30f520320b0f55fcdd76e1
AaronGuapo1 <89491989+AaronGuapo1@users.noreply.github.com>
2022-04-04 16:19:04 -0500

    main

--- routes/login.js
+++ routes/login.js
@@ -3,63 +3,79 @@ var router = express.Router();
 var {client,dbName} = require('../db/mongo');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
+var bcrypt = require('bcryptjs');
+
 
 
 passport.use(new LocalStrategy(
   async function(username, password, done) {
-
+    const pstatus = await
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection('usuarios');
     await collection.findOne({ usuario: username }, function (err, user) {
-
-
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      console.log(password);
-      console.log(user.password);
-      if (password!==user.password) { return done(null, false); }
+      console.log(""+bcrypt.compare(password, user.password, function(err,res){
+        return res
+      }));
+
+      bcrypt.compare(password, user.password).then((res) => {
+        // res === true
+        if (!res) {return done(null, false);}
       return done(null, user);
+    });
+      
     });
   }
 ));
+
+
+
 
 passport.serializeUser(function(user, done) {
   console.log(user);
   done(null, user.usuario);
 });
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('login');
 });
 
-router.post('/registro', async function(req, res){
+
+router.post('/registro', async function(req, res,next){
 
   regUser(req.body)
-  
-    .then(()=>{
-      res.render('login',{info: "Registrado"})
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-    .finally(()=>{
-      client.close()
-    })
+
+.then(()=>{
+  res.render('login',{info: "Registrado"})
+})
+
+.catch((err)=>{
+  console.log(err);
+})
+.finally(()=>{
+  client.close()
+})
+
 
 });
 
-async function regUser(datos){
 
+
+
+
+
+async function regUser(datos){
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection('usuarios');
+  const hashUserPassword = await bcrypt.hash(datos.password, 10)
   await collection.insertOne(
     {
       usuario: datos.usuario,
-      password: datos.password
-
+      password: hashUserPassword
     }
   )
 }
@@ -75,19 +91,3 @@ router.post('/login',
 module.exports = router;
 
 
-
-
-
-/*
-      password: passwordHash
-
-        var password = datos.password;
-  let passwordHash= bcryptjs.hashSync(password,9);
-
-  
-const app = express();
-const bcryptjs = require('bcryptjs');
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-
-*/
--- routes/pruebas.js
+++ routes/pruebas.js
@@ -1,39 +1,94 @@
 /*
-const app = express();
-const bcryptjs = require('bcryptjs');
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+var express = require('express');
+var router = express.Router();
+var {client,dbName} = require('../db/mongo');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
-const usuario1 = req.body.usuario;
-const password1 = req.body.password;
-if (usuario1 == usuario && password1 == password ){
-let passwordHash= bcryptjs.hashSync(password,9);
-console.log("It worked")
-    console.log((passwordHash));
-    console.log(password)
+var bcrypt = require('bcryptjs');
+
+
+
+passport.use(new LocalStrategy(
+  async function(username, password, done,user) {
+
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('usuarios');
+    await collection.findOne({ usuario: username }, function (err, user) {
+
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      console.log(password);
+      console.log(user.password);
+      if (password!==user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+
+
+
+passport.serializeUser(function(user, done) {
+  console.log(user);
+  done(null, user.usuario);
+});
+
+
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+
+router.post('/registro', async function(req, res,next){
+
+  regUser(req.body)
+
+
+    .then(()=>{
+      res.render('login',{info: "Registrado"})
+    })
+
+    .catch((err)=>{
+      console.log(err);
+    })
+    .finally(()=>{
+      client.close()
+    })
+
+});
+
+
+
+
+async function regUser(datos){
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection('usuarios');
+
+  await collection.insertOne(
+    {
+      usuario: datos.usuario,
+      password: datos.password
+    }
+  )
 }
-else {
-console.log("it didn't work")
-}
 
-Funciona
-    const password = req.body.password;
-    let passwordHash= bcryptjs.hashSync(password,9);
-    
-    console.log(passwordHash);
+router.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
 
 
-pegar
-  const password = req.body.password;
-  const passwordHash=
+    res.redirect('/');
+  });
 
-console.log(password)
-      password: datos.passwordHash
+module.exports = router;
 
 
-  const password = req.body.password;
-  let passwordHash= bcryptjs.hashSync(password,9);
-  console.log(passwordHash);
-  console.log(password);
+
+
 
 */
+
+  
